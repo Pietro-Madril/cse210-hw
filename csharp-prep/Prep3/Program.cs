7c21c4e1d9ca3f5ceee26e878107b247ca473bb4
Pietro-Madril <pietromotta.madril@gmail.com>
2024-05-04 18:41:43 -0300

    prep 3 finish

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        // //User defined guess loop - turn off the comments deleting the "//" in the lines 9 to 11
+        // Console.WriteLine("What is the secret number?");
+        // string yourNumber = Console.ReadLine();
+        // int Number = int.Parse(yourNumber);
+        
+        Random randomNumber = new Random();
+        int Number = randomNumber.Next(1, 20);
+
+        int guess = -1;
+        while (guess != Number)    
+        {   
+            Console.WriteLine("What is your guess?");
+            string yourGuess = Console.ReadLine();
+            guess = int.Parse(yourGuess);
+            if ( guess > Number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < Number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Your guess are correct");
+            }
+        }
     }
 }
